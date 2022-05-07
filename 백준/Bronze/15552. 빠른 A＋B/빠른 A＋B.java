56dd1aa1fc87ad822d57d1bd1bf9014c5a7b02bd
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-05-07 17:52:22 +0900

    Update 빠른 A＋B.java

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -3,18 +3,17 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+            int testcase = Integer.parseInt(reader.readLine());
 
-        StringTokenizer st;
+            for(int i=0; i<testcase; i++){
+                StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+                writer.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())+"\n"));
+            }
+            writer.flush();
+            writer.close();
 
-        for(int i=0; i<t; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())+"\n"));
-        }
-        bw.flush();
-        bw.close();
     }
 }
