1a2e249d726adbcb53158e61d3c548c0209fae0a
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-05-15 01:46:12 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String[] str = new String[] {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
-        
+
         int[] strToInt = new int[str.length];
         for(int i=0; i<str.length; i++){
             strToInt[i] = i+3;
         }
 
-        Scanner sc = new Scanner(System.in);
 
-        String dial = sc.next();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String dial = br.readLine();
 
         char[] dialArr = new char[dial.length()];
 
         for(int i=0; i<dialArr.length; i++) {
             dialArr[i] = dial.charAt(i);
         }
-        
+
         int time = 0;
 
         for(int i=0; i<str.length; i++){
