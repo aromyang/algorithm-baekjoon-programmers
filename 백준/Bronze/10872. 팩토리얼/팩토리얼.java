ad939e5f5d73a21f032973cdc7f46566ca536bd0
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-05-24 10:40:36 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(reader.readLine());
         System.out.println(factorial(num));
+        
     }
     static int factorial(int num){
         if(num==0) return 1;
