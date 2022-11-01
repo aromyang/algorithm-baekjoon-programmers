22f0abaeaa7b5e7f5989f0ff848ed979128d84a3
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-01 13:30:40 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.54 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String lower = my_string.toLowerCase();
+        char[] chars = lower.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+}
