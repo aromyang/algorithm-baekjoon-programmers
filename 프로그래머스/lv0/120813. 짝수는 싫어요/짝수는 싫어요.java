ca901cc593030317a7d47a66cd1d4c021b6cd01e
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-01 11:05:51 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        if (n % 2 == 0) {
+            answer = new int[n / 2];
+        } else {
+            answer = new int[(n / 2) + 1];
+        }
+
+        answer[0] = 1;
+        if (answer.length == 1) return answer;
+        
+        for (int i = 1; i < answer.length; i ++) {
+            answer[i] += answer[i-1] + 2;
+        }
+        
+        return answer;
+    }
+}
