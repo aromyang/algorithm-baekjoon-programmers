d3b1afd5dbe6614a4c87ce03428322b21347e99d
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-07 00:02:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for (String[] s : db) {
+            if (s[0].equals(id_pw[0]) && s[1].equals(id_pw[1])) {
+                return "login";
+            } else if (s[0].equals(id_pw[0])) {
+                return "wrong pw";
+            }
+        }
+        return "fail";
+    }
+}
