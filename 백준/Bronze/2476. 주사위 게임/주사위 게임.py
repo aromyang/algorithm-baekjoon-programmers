b9dcb4c12d40922a864aed6f2e9b1af0ef32ba6d
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-14 19:29:49 +0900

    [Bronze III] Title: 주사위 게임, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,15 @@
+n = int(input())
+answer = 0
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    if a == b == c:
+        answer = max(answer, 10000 + (a * 1000))
+    elif a == b:
+        answer = max(answer, 1000 + (a * 100))
+    elif b == c:
+        answer = max(answer, 1000 + (b * 100))
+    elif a == c:
+        answer = max(answer, 1000 + (a * 100))
+    else:
+        answer = max(answer, max(a, b, c) * 100)
+print(answer)
