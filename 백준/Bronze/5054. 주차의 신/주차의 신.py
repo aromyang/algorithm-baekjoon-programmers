c2a28eeca99b7cb3e0aafbecf8f05f0669089753
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-15 14:09:54 +0900

    [Bronze II] Title: 주차의 신, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5054. 주차의 신/주차의 신.py
+++ 백준/Bronze/5054. 주차의 신/주차의 신.py
@@ -1,9 +1,5 @@
 t = int(input())
 for _ in range(t):
     a = int(input())
-    l = list(map(int, input().split()))
-    d = 0
-    l.sort(reverse=True)
-    for i in range(a - 1):
-        d += l[i] - l[i + 1]
-    print(d * 2)
+    l = sorted(map(int, input().split()))
+    print((l[-1] - l[0]) * 2)
