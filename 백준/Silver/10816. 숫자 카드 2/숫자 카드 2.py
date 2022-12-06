cec54d2ec8add16dfffc48d1a557bb40c5c13d23
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-12-06 23:35:05 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 816 ms, Memory: 118720 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l1 = list(map(int, input().split()))
+m = int(input())
+l2 = list(map(int, input().split()))
+
+cnt = {}
+for i in l1:
+    if i in cnt:
+        cnt[i] += 1
+    else:
+        cnt[i] = 1
+
+for i in l2:
+    if i in cnt:
+        print(cnt[i], end=' ')
+    else:
+        print(0, end=' ')
+        
