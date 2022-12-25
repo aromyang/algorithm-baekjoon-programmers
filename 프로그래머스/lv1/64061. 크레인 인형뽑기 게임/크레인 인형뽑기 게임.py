8616aefd62927a8270bedbc82207dfe3c73503f3
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-12-25 23:43:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,14 +1,3 @@
 def solution(board, moves):
-    stack = []
     answer = 0
-    for m in moves:
-        for i in range(len(board)):
-            if board[i][m - 1] != 0:
-                stack.append(board[i][m - 1])
-                board[i][m - 1] = 0
-                break
-        if len(stack) >= 2 and stack[-1] == stack[-2]:
-            stack.pop()
-            stack.pop()
-            answer += 2
     return answer
