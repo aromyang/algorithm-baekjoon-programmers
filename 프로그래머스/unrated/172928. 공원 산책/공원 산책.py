323c64a3a7afd5b6431c93c3579d077b07204b78
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-02 00:28:44 +0900

    [unrated] Title: 공원 산책, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,42 @@
+def solution(park, routes):
+    now = ()
+    row = len(park)
+    col = len(park[0])
+
+    while not now:
+        for i in range(row):
+            for j in range(col):
+                if park[i][j] == 'S':
+                    now = (i, j)
+
+    for r in routes:
+        direction, distance = r.split()
+        distance = int(distance)
+        r, c = now[0], now[1]
+        if direction == 'N' and now[0] - distance >= 0:
+            for _ in range(distance):
+                r -= 1
+                if park[r][c] == 'X':
+                    r = now[0]
+                    break
+        elif direction == 'S' and now[0] + distance < row:
+            for _ in range(distance):
+                r += 1
+                if park[r][c] == 'X':
+                    r = now[0]
+                    break
+        elif direction == 'W' and now[1] - distance >= 0:
+            for _ in range(distance):
+                c -= 1
+                if park[r][c] == 'X':
+                    c = now[1]
+                    break
+        elif direction == 'E' and now[1] + distance < col:
+            for _ in range(distance):
+                c += 1
+                if park[r][c] == 'X':
+                    c = now[1]
+                    break
+        now = (r, c)
+
+    return now
