5c71e98f5063293d5cf46c2423f7a2eaa34e7aad
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-02 02:10:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 328 ms, Memory: 50120 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,40 +1,26 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 input = sys.stdin.readline
 
-
-class Tree:
-    def __init__(self, root):
-        self.tree = {root: []}
-
-    def append_node(self, parent, child):
-        if parent not in self.tree:
-            self.tree[parent] = []
-        if child not in self.tree:
-            self.tree[child] = []
-        self.tree[parent].append(child)
-        self.tree[child].append(parent)
-
-    def get_nodes(self, root):
-        return self.tree[root]
-
-    def dfs(self, cur_node, root_node, ans):
-        ans[cur_node] = root_node
-        for node in self.get_nodes(cur_node):
-            if node != root_node:
-                self.dfs(node, cur_node, ans)
-
-
 n = int(input())
-tree = Tree(1)
-
+graph = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
     node1, node2 = map(int, input().split())
-    tree.append_node(node1, node2)
-
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+    
+queue = deque([1])
 ans = [0] * (n + 1)
-tree.dfs(1, 0, ans)
+visited = [False] * (n + 1)
+while queue:
+    cur_node = queue.popleft()
+    for next_node in graph[cur_node]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            ans[next_node] = cur_node
+            queue.append(next_node)
 
 for i in range(2, n + 1):
     print(ans[i])
+    
