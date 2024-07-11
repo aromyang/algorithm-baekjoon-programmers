4c1176763cdae84ec35183636718193b85b1c059
아롬 <85007104+aromyang@users.noreply.github.com>
2024-07-11 21:38:43 +0900

    [level 2] Title: 괄호 변환, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,7 +1,6 @@
 def solution(p):
     def seperate(p):
         balance_cnt = 0
-        
         for i, pp in enumerate(p):
             if i != 0 and balance_cnt == 0:
                 return p[:i], p[i:]
@@ -9,12 +8,10 @@ def solution(p):
                 balance_cnt += 1
             else:
                 balance_cnt -= 1
-        
         return p, ''
     
     def is_correct(p):
         correct = []
-        
         for pp in p:
             if pp == '(':
                 correct.append(pp)
@@ -22,8 +19,7 @@ def solution(p):
                 if not correct:
                     return False
                 correct.pop()
-                
-        return True
+        return len(correct) == 0
     
     def reverse(p):
         return ''.join(')' if c == '(' else '(' for c in p)
@@ -39,6 +35,4 @@ def solution(p):
         else:
             return '(' + make_correct(v) + ')' + reverse(u[1:len(u)-1])
     
-    answer = ''
-    answer += make_correct(p)
-    return answer
+    return make_correct(p)
